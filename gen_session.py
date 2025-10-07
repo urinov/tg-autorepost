69685b5a3167343c69acd0560021885d77ab03ee
urinov <30720428+urinov@users.noreply.github.com>
2025-10-07 14:49:55 +0500

    Update gen_session.py

--- gen_session.py
+++ gen_session.py
@@ -1,1 +1,9 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
 
+API_ID = int(input("API_ID: "))
+API_HASH = input("API_HASH: ")
+
+with TelegramClient(StringSession(), API_ID, API_HASH) as client:
+    print("\n👉 YOUR STRING SESSION (copy it):")
+    print(client.session.save())
